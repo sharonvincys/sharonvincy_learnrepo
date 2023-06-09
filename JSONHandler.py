0b6e752d3c42eecaa01afa9b569db7ee728b81bc
sharonvincys <134835496+sharonvincys@users.noreply.github.com>
2023-06-09 22:48:10 +0530

    Create JSONHandler.py

---
+++ JSONHandler.py
@@ -0,0 +1,24 @@
+import json
+file_name = r'''C:\Users\Bob\Desktop\example.txt'''
+with open(file_name, 'r', encoding='utf-8') as file:
+
+    ex5 = json.load(file)
+
+    for i in ex5:
+        if i['name'] == 'Old Fashioned' and i['type'] == 'donut':
+            x = i['batters']['batter']
+            for id in x:
+                cur_id = id["id"]
+                last_id = cur_id
+            last_id=int(last_id) + 1
+            z={ "id": last_id, "type": "Coffee" }
+            x.append(z)
+    print(ex5)
+        
+
+file_name = r'''C:\Users\Bob\Desktop\example.txt'''
+with open(file_name, 'w', encoding='utf-8') as out:
+    json.dump(ex5,out,indent=4)
+
+
+
